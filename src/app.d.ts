c95e428ae6fc2ccd4b50cf1c32c3ed795078d0d0
Brice <hi@bnema.dev>
2024-03-14 05:23:41 +0100

    Fix missing propriety for Lucia

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,11 +2,10 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Locals {
+			user: import("lucia").User | null;
+			session: import("lucia").Session | null;
+		}
 	}
 }
 
