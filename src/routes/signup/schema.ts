18c2c4234164a09c4f4ae5f55bda5cb2f60a3450
Brice <hi@bnema.dev>
2024-03-20 06:37:30 +0100

    fix zod signup schema

--- src/routes/signup/schema.ts
+++ src/routes/signup/schema.ts
@@ -1,14 +1,20 @@
 import { z } from "zod";
 
+const passwordSchema = z.string().min(8).max(50)
+    .refine((password) => /[A-Z]/.test(password), {
+        message: "Password must contain at least one uppercase letter.",
+    })
+    .refine((password) => /[a-z]/.test(password), {
+        message: "Password must contain at least one lowercase letter.",
+    })
+    .refine((password) => /\d/.test(password), {
+        message: "Password must contain at least one number.",
+    });
+
 export const formSchema = z.object({
     email: z.string().email().max(50),
-    password: z.string().min(8).max(50).refine((password) => {
-        const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d]{8,}$/;
-        return passwordPattern.test(password);
-    }, {
-        message: "Password must contain at least 8 characters, one uppercase letter, one lowercase letter, and one number."
-    }),
-    confirmPassword: z.string().min(8).max(50),
+    password: passwordSchema,
+    confirmPassword: passwordSchema,
 }).refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match.",
     path: ["confirmPassword"],
